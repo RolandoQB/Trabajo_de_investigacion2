c36191f099cebbe8b8248eaf84a17accf5a4de91
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-07-29 00:27:21 -0500

    Update operaciones_de_cadena.py

--- operaciones_de_cadena.py
+++ operaciones_de_cadena.py
@@ -4,10 +4,24 @@ class Cadena():
 
 #___________________________________________________________________________________________________________   
     def  recorrerCadena(self):
-        pass
+        print(':::::::::::::::::::::::::::::::::::::::::::::')
+        print("Recorrer y presentar los datos de una cadena")
+        print(':::::::::::::::::::::::::::::::::::::::::::::')
+        for x in self.cadena:
+            print(x,'',end='')
+        
 #___________________________________________________________________________________________________________  
     def  buscarCaracter(self,buscado):
-        pass
+        print(':::::::::::::::::::::::::::::::::::::')
+        print("Buscar un carácter en una cadena")
+        print(':::::::::::::::::::::::::::::::::::::')
+        acum=0
+        for x,i in enumerate(self.cadena):
+            if i== buscado:
+                acum=acum+1
+        print("Su caracter se encuentra {} veces, dentro de la cadena".format(acum))
+        print()
+        
 #___________________________________________________________________________________________________________  
     def  listaPosiciones(self,caracter):
         pass
@@ -31,5 +45,7 @@ class Cadena():
         pass
 #___________________________________________________________________________________________________________         
 
-
+cadena='hola como esta me miro y acercandose no lo podia creer'
 cad= Cadena(cadena)
+# cad.recorrerCadena()
+cad.buscarCaracter('b')
