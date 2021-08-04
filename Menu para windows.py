b9bc8789bfe317054598b35dda0cc72b3390164e
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-08-03 23:25:51 -0500

    Update Menu para windows.py

--- Menu para windows.py
+++ Menu para windows.py
@@ -29,7 +29,7 @@ while opc != "5":
             opc1 = men1.menu()
             if opc1 == "1":
                 os.system("cls")
-                print("<----Ingrese los numeros para la respectiva suma---->")
+                print("░░░░░░░░░░Ingrese los numeros para la respectiva suma░░░░░░░░░░")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = CalEstandar(n1,n2)
@@ -38,7 +38,7 @@ while opc != "5":
                 
             elif opc1 == "2":
                 os.system("cls")
-                print("<----Ingrese los numeros para la respectiva resta---->")
+                print("░░░░░░░░░░Ingrese los numeros para la respectiva resta░░░░░░░░░░")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = CalEstandar(n1,n2)
@@ -47,7 +47,7 @@ while opc != "5":
                 
             elif opc1 == "3":
                 os.system("cls")
-                print("<----Ingrese los numeros para la respectiva multiplicacion---->")
+                print("░░░░░░░░░░Ingrese los numeros para la respectiva multiplicacion░░░░░░░░░░")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = CalEstandar(n1,n2)
@@ -56,7 +56,7 @@ while opc != "5":
              
             elif opc1 == "4":
                 os.system("cls")
-                print("<----Ingrese los numeros para la respectiva division---->")
+                print("░░░░░░░░░░Ingrese los numeros para la respectiva division░░░░░░░░░░")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = CalEstandar(n1,n2)
@@ -65,7 +65,6 @@ while opc != "5":
                 
             elif opc1 == "5":
                 os.system("cls")
-                print("<----Exponentes---->")
                 n1 = int(input("Ingrese el primer valor como base:"))
                 n2 = int(input("Ingrese el segundo valor como exponente:"))
                 cal = CalEstandar(n1,n2)
@@ -74,7 +73,6 @@ while opc != "5":
                 
             elif opc1 == "6":
                 os.system("cls")
-                print("<----Valor Absoluto---->")
                 n1 = int(input("Ingrese el numero:"))
                 cal = CalEstandar(0,0)
                 print("El Valor Absoluto es: ",cal.valorAbsoluto(n1))
@@ -82,7 +80,6 @@ while opc != "5":
             
             elif opc1 == "7":
                 os.system("cls")
-                print("<----Circunferencia---->")
                 n1 = int(input("Ingrese el valor del radio:"))
                 cal = calCientifica(n1,0)
                 print("El perimetro de la circunferencia es: ",cal.circunferencia())
@@ -90,7 +87,6 @@ while opc != "5":
                 
             elif opc1 == "8":
                 os.system("cls")
-                print("<----Area del Circulo---->")
                 n1 = int(input("Ingrese el valor del radio:"))
                 cal = calCientifica(n1,0)
                 print("El area del Circulo es: ",cal.areaCirculo())
@@ -98,7 +94,6 @@ while opc != "5":
                 
             elif opc1 == "9":
                 os.system("cls")
-                print("<----Area del Cuadrado---->")
                 n2 = int(input("Ingrese el valor del lado:"))
                 cal = calCientifica(0,n2)
                 print("El area del cuadrado es: ",cal.areaCuadrado())
@@ -112,7 +107,6 @@ while opc != "5":
             opc2 = men2.menu()
             if opc2 == "1":
                 os.system("cls")
-                print("<----Presentar los números de 1 a n---->")
                 n1 = int(input("Ingrese el numero hasta donde desea presentar:"))
                 cal = Basico()
                 cal.numerosN(n1)
@@ -120,7 +114,7 @@ while opc != "5":
                 
             elif opc2 == "2":
                 os.system("cls")
-                print("<----Múltiplo de cualquier numero---->")
+                print("▌│█║▌║▌║ Múltiplo de cualquier numero ║▌║▌║█│▌")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = Basico()
@@ -129,7 +123,7 @@ while opc != "5":
                 
             elif opc2 == "3":
                 os.system("cls")
-                print("<----Presentar los divisores de un numero---->")
+                print("▌│█║▌║▌║ Presentar los divisores de un numero ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el primer valor:"))
                 cal = Basico()
                 print("La respuesta es: ",cal.divisoresNumero(n1))
@@ -137,7 +131,7 @@ while opc != "5":
                 
             elif opc2 == "4":
                 os.system("cls")
-                print("<----Numero Primo---->")
+                print("▌│█║▌║▌║ Numero Primo ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el valor:"))
                 cal = Basico()
                 cal.primo(n1)
@@ -145,7 +139,7 @@ while opc != "5":
                 
             elif opc2 == "5":
                 os.system("cls")
-                print("<----Perfecto--->")
+                print("▌│█║▌║▌║ Perfecto ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el valor:"))
                 cal = Basico()
                 cal.perfecto(n1)
@@ -153,7 +147,7 @@ while opc != "5":
                 
             elif opc2 == "6":
                 os.system("cls")
-                print("<----Factorial--->")
+                print("▌│█║▌║▌║ Factorial ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el valor:"))
                 cal = Intermedio()
                 print(cal.factorial(n1))
@@ -161,7 +155,7 @@ while opc != "5":
                 
             elif opc2 == "7":
                 os.system("cls")
-                print("<----Fibonacci--->")
+                print("▌│█║▌║▌║ Fibonacci ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el valor:"))
                 cal = Intermedio()
                 print(cal.fibonacci(n1))
@@ -169,7 +163,7 @@ while opc != "5":
                 
             elif opc2 == "8":
                 os.system("cls")
-                print("<----Primos Gemelos--->")
+                print("▌│█║▌║▌║ Primos Gemelos ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = Intermedio()
@@ -178,7 +172,7 @@ while opc != "5":
                 
             elif opc2 == "9":
                 os.system("cls")
-                print("<----Numeros Amigos--->")
+                print("▌│█║▌║▌║ Numeros Amigos ▌│█║▌║▌║")
                 n1 = int(input("Ingrese el primer valor:"))
                 n2 = int(input("Ingrese el segundo valor:"))
                 cal = Intermedio()
@@ -193,7 +187,7 @@ while opc != "5":
             opc3 = men3.menu()   
             if opc3 == "1":
                 os.system("cls")
-                print("<----Presentar lista--->")      
+                print("░░░░░░░░░░Presentar lista░░░░░░░░░░")      
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 lista = []
                 for x in range(num):
@@ -206,7 +200,7 @@ while opc != "5":
             
             elif opc3 == "2":
                 os.system("cls")
-                print("<----Buscar Lista--->")
+                print("░░░░░░░░░░Buscar Lista░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -220,7 +214,7 @@ while opc != "5":
                 
             elif opc3 == "3":
                 os.system("cls")
-                print("<----Lista Factorial--->")
+                print("░░░░░░░░░░Lista Factorial░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -232,7 +226,7 @@ while opc != "5":
                 
             elif opc3 == "4":
                 os.system("cls")
-                print("<----Lista Numeros Primos--->")
+                print("░░░░░░░░░░Lista Numeros Primos░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -244,14 +238,14 @@ while opc != "5":
                 
             elif opc3 == "5":
                 os.system("cls")
-                print("<----Lista Notas--->")
+                print("░░░░░░░░░░Lista Notas░░░░░░░░░░")
                 diccionario=[{'nombre':'Josue', 'nota':100},{'nombre':'Mario','nota':80},{'nombre':'Miguel','nota':90}]
                 resultado = TratamientoListas(diccionario)
                 resultado.listaNotas()
                 
             elif opc3 == "6":
                 os.system("cls")
-                print("<----Insertar Lista--->")
+                print("░░░░░░░░░░Insertar Lista░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -265,7 +259,7 @@ while opc != "5":
                 
             elif opc3 == "7":
                 os.system("cls")
-                print("<----Eliminar Lista--->")
+                print("░░░░░░░░░░Eliminar Lista░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -277,7 +271,7 @@ while opc != "5":
                 
             elif opc3 == "8":
                 os.system("cls")
-                print("<----Retornar valor lista--->")
+                print("░░░░░░░░░░Retornar valor lista░░░░░░░░░░")
                 lista = []       
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 for i in range(num):
@@ -289,14 +283,14 @@ while opc != "5":
                 
             elif opc3 == "9":
                 os.system("cls")
-                print("<----Copiar tupla--->")
+                print("░░░░░░░░░░Copiar tupla░░░░░░░░░░")
                 tupla = (2,2,3,4,5,6,7)
                 resultado = TratamientoListas(tupla)
                 print(resultado.copiarTuplaLista())
                 
             elif opc3 == "10":
                 os.system("cls")
-                print("<----Vuelto lista--->")
+                print("░░░░░░░░░░Vuelto lista░░░░░░░░░░")
                 diccionario={}
                 lista=[]
                 num=int(input("ingrese cuantos diccionarios desea ingresar: "))
@@ -319,7 +313,7 @@ while opc != "5":
             opc4 = men4.menu()
             if opc4 == "1":
                 os.system("cls")
-                print("<---Recorrer Cadena---->")
+                print("░░░░░░░░░░Recorrer Cadena░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 resultado = Cadena(Texto)
                 resultado.recorrerCadena()
@@ -327,7 +321,7 @@ while opc != "5":
                 
             elif opc4 == "2":
                 os.system("cls")
-                print("<---Buscar Caracter---->")
+                print("░░░░░░░░░░Buscar Caracter░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 buscar = input("Ingrese el caracter que desea buscar:")
                 resultado = Cadena(Texto)
@@ -336,7 +330,7 @@ while opc != "5":
                 
             elif opc4 == "3":
                 os.system("cls")
-                print("<---Lista Posiciones---->")
+                print("░░░░░░░░░░Lista Posiciones░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 buscar = input("Ingrese el caracter:")
                 resultado = Cadena(Texto)
@@ -345,7 +339,7 @@ while opc != "5":
                 
             elif opc4 == "4":
                 os.system("cls")
-                print("<---Lista Palabras---->")
+                print("░░░░░░░░░░Lista Palabras░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 resultado = Cadena(Texto)
                 resultado.listaPalabras()
@@ -353,7 +347,7 @@ while opc != "5":
                 
             elif opc4 == "5":
                 os.system("cls")
-                print("<---Lista Palabras---->")
+                print("░░░░░░░░░░Lista Palabras░░░░░░░░░░")
                 lista=[]
                 num = int(input("Cuantos elementos desea en la lista?: "))
                 lista = []
@@ -370,7 +364,7 @@ while opc != "5":
                 
             elif opc4 == "6":
                 os.system("cls")
-                print("<---Insertar Dato---->")
+                print("░░░░░░░░░░Insertar Dato░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 texto_insertar = input("Ingrese el texto a insertar:")
                 posicion_insertar = int(input("Ingrese en que posicion desea insertar el texto:"))
@@ -380,7 +374,7 @@ while opc != "5":
             
             elif opc4 == "7":
                 os.system("cls")
-                print("<---Eliminar Ocurrencias---->")
+                print("░░░░░░░░░░Eliminar Ocurrencias░░░░░░░░░░")
                 Texto = input("Ingrese la Cadena:")
                 Buscado = input("Ingrese el elemento a buscar:")
                 resultado = Cadena(Texto)
@@ -389,7 +383,7 @@ while opc != "5":
         
             elif opc4 == "8":
                 os.system("cls")
-                print("<---Retornar Valor---->")
+                print("░░░░░░░░░░Retornar Valor░░░░░░░░░░")
                 texto = input("Ingrese el texto:")        
                 res = Cadena(texto)
                 pos = int(input("Ingrese posicion:"))
@@ -398,7 +392,7 @@ while opc != "5":
                 
             elif opc4 == "9":
                 os.system("cls")
-                print("<---Retornar Valor---->")
+                print("░░░░░░░░░░Retornar Valor░░░░░░░░░░")
                 texto = input("Ingrese su nombre:")        
                 res = Cadena(texto)
                 res.concatenarCadena(texto)  
